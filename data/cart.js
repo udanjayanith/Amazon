412c2dcbf5084a8066bef58c196c44260d435ccf
udan jayanith <j.udanjayanith@gmail.com>
2024-03-19 21:01:24 +0530

    test

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-let cart = []
+export let cart = []
 
-function cartLength(){
+export function cartLength(){
     if (cart.length == 0) cartNum.innerHTML = ''
     else cartNum.innerHTML = cart.length
   }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,4 @@
+import { cart, cartLength } from "../data/cart"
 
 if(JSON.parse(localStorage.getItem('products')) == null) console.log('null')
 //else cart = JSON.parse(localStorage.getItem('products'))
