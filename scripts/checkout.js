41eddc910f4a5d6d94748d488eb0865cd1efbc6d
udan jayanith <j.udanjayanith@gmail.com>
2024-03-20 18:19:14 +0530

    add discription to products in cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,21 +14,22 @@ cart.forEach(itemsInCart => {
     products.forEach(product => {
         if(product.id == itemsInCart.id) {
             itemsInCartRender(product, itemsInCart)
-            
+
             typeOfItems.innerHTML = cart.length
             items.innerHTML = `Items (${typeOfItems.innerHTML}):`
             
             num +=((product.prize*itemsInCart.quantity)/100)
             paymentSummaryMoney.innerHTML = num.toFixed(2)
-
+          
             if(num.toFixed(2) > 100) tax = 6
             else tax = 4
             shippingAndHandling.innerHTML = (num*(tax/100)).toFixed(2)
             beforeTax.innerHTML = (num *(tax*2/100)).toFixed(2)
-
+          
             estimatedTax.innerHTML = (num*(10/100)).toFixed(2)
-
+          
             orderTotal.innerHTML = (num+(num*(tax/100))+(num *(tax*2/100))+(num*(10/100))).toFixed(2)
+
         }
         
     })
@@ -69,50 +70,17 @@ function itemsInCartRender(product, itemsInCart){
 
       <div class="delivery-options">
         <div class="delivery-options-title">
-          Choose a delivery option:
+        Description:
         </div>
-        <div class="delivery-option">
-          <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-1">
-          <div>
-            <div class="delivery-option-date">
-              Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-              FREE Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-1">
-          <div>
-            <div class="delivery-option-date">
-              Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-              $4.99 - Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-1">
-          <div>
-            <div class="delivery-option-date">
-              Monday, June 13
-            </div>
-            <div class="delivery-option-price">
-              $9.99 - Shipping
-            </div>
-          </div>
+
+        <p class="limit">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minima corrupti nobis vel similique soluta cupiditate placeatsectetur adipisicing elit.</p>
+
         </div>
       </div>
+
     </div>
   </div>
     
     `
+   
 }
